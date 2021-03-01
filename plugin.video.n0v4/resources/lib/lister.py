26f6f029c87e4f8f844e240b93db7c21820e6077
Threshold84 <threshold84@protonmail.com>
2021-03-01 12:14:18 +0200

    Update n0v4

--- plugin.video.n0v4/resources/lib/lister.py
+++ plugin.video.n0v4/resources/lib/lister.py
@@ -13,7 +13,7 @@ def _get_content():
     result = []
     futures = []
 
-    html = client.request('https://www.dailyiptvlist.com/european-m3u-iptv/greece-greek/')
+    html = client.request('https://www.dailyiptvlist.com/euro-free-m-3u-ip-tv/greece-greek/')
 
     latest = itertags_wrapper(html, 'a', {'class': 'image-link'}, 'href')[0]
 
